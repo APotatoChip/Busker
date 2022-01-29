76a2e048dbddf23551ae435d52edf55def5ec0fe
APotatoChip <cvetina.a.m@gmail.com>
2022-01-30 01:34:40 +0200

    profile changed text color and create post deisign

--- NgBusker/src/app/post/post.module.ts
+++ NgBusker/src/app/post/post.module.ts
@@ -6,20 +6,25 @@ import { PostComponent } from './post/post.component';
 import { CommentsModule } from '../comments/comments.module';
 import { CommentComponent } from '../comments/comment/comment.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 
 @NgModule({
   declarations: [
-    PostComponent,PostListItemComponent
+    PostComponent,
+    PostListItemComponent,
+    NewPostComponent
   ],
   imports: [
     CommonModule,
     PostRoutingModule,
     CommentsModule
+  
   ],
   exports:[
     PostComponent,
-    PostListItemComponent
+    PostListItemComponent,
+    NewPostComponent
   ]
 })
 export class PostModule { }
