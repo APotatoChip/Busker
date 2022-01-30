b9adac772c25f33b60c83b647ea82bcf9cc0b31f
APotatoChip <cvetina.a.m@gmail.com>
2022-01-30 02:01:12 +0200

    different profile views

--- NgBusker/src/app/profiles/busker/busker.component.ts
+++ NgBusker/src/app/profiles/busker/busker.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  faInstagram, faFacebook, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-busker',
   templateUrl: './busker.component.html',
   styleUrls: ['./busker.component.css']
 })
 export class BuskerComponent implements OnInit {
-
+  faInstagram = faInstagram;
+  faFacebook=faFacebook;
+  faTwitter=faTwitter;
+  faYoutube=faYoutube;
   constructor() { }
 
   ngOnInit(): void {
